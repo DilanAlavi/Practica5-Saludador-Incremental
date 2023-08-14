134d1cf6ce31caaeb389e10d237d35ee8edc00e7
DILAN ALVARO ALAVI GUTIERREZ <dilanalavi11@gmail.com>
2023-08-13 22:37:49 -0400

    Añadiendo Saludar por Nombre

--- src/main.js
+++ src/main.js
@@ -2,14 +2,21 @@ import { mostrarMensaje } from "./mensaje.js";
 
 document.addEventListener("DOMContentLoaded", function() {
     const miBoton = document.getElementById("miBoton");
+    const nombreInput = document.getElementById("nombreInput");
 
     miBoton.addEventListener("click", function() {
-        mostrarMensaje("Hola");
+        const nombre = nombreInput.value;
+        if (nombre.trim() !== "") {
+            mostrarMensaje(`Hola, ${nombre}!`);
+        }
     });
 
     document.addEventListener("keydown", function(event) {
         if (event.key === "Enter") {
-            mostrarMensaje("Hola");
+            const nombre = nombreInput.value;
+            if (nombre.trim() !== "") {
+                mostrarMensaje(`Hola, ${nombre}!`);
+            }
         }
     });
 });
