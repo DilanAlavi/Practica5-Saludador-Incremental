497e1ddb562c1157121416b590f95291eeef1267
DILAN ALVARO ALAVI GUTIERREZ <dilanalavi11@gmail.com>
2023-08-14 00:05:57 -0400

    Añadir funcion Saludar con el GENERO

--- src/main.js
+++ src/main.js
@@ -1,22 +1,42 @@
 import { mostrarMensaje } from "./mensaje.js";
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const miBoton = document.getElementById("miBoton");
     const nombreInput = document.getElementById("nombreInput");
 
-    miBoton.addEventListener("click", function() {
+    miBoton.addEventListener("click", async function () {
         const nombre = nombreInput.value;
         if (nombre.trim() !== "") {
-            mostrarMensaje(`Hola, ${nombre}!`);
+            const genero = await determinarGenero(nombre);
+            mostrarMensaje(nombre, genero);
         }
     });
 
-    document.addEventListener("keydown", function(event) {
+    document.addEventListener("keydown", async function (event) {
         if (event.key === "Enter") {
             const nombre = nombreInput.value;
             if (nombre.trim() !== "") {
-                mostrarMensaje(`Hola, ${nombre}!`);
+                const genero = await determinarGenero(nombre);
+                mostrarMensaje(nombre, genero);
             }
         }
     });
+
+    async function determinarGenero(nombre) {
+        try {
+            const response = await fetch(`https://api.genderize.io/?name=${nombre}`);
+            const data = await response.json();
+            
+            if (data.gender === "male") {
+                return "Señor";
+            } else if (data.gender === "female") {
+                return "Señora";
+            } else {
+                return "Persona";
+            }
+        } catch (error) {
+            console.error("Error al determinar el género:", error);
+            return "Desconocido";
+        }
+    }
 });
--- src/mensaje.js
+++ src/mensaje.js
@@ -1,6 +1,6 @@
-export function mostrarMensaje(texto) {
+export function mostrarMensaje(nombre, genero) {
     const mensajeDiv = document.getElementById("mensaje");
     const nuevoMensaje = document.createElement("p");
-    nuevoMensaje.innerHTML = `<strong>${texto}</strong>`;
+    nuevoMensaje.innerHTML = `<strong>Hola, ${genero} ${nombre}!</strong>`;
     mensajeDiv.appendChild(nuevoMensaje);
 }
