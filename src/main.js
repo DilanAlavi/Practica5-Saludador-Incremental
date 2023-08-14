2f0e65b1e2452941b5438eee9aa8951157272593
DILAN ALVARO ALAVI GUTIERREZ <dilanalavi11@gmail.com>
2023-08-14 13:04:41 -0400

    Añadiendo el cambio de Idioma

--- src/main.js
+++ src/main.js
@@ -4,14 +4,77 @@ document.addEventListener("DOMContentLoaded", function () {
     const miBoton = document.getElementById("miBoton");
     const nombreInput = document.getElementById("nombreInput");
     const edadInput = document.getElementById("edadInput");
+    const idiomaSelector = document.getElementById("idiomaSelector");
+
+    // Objeto de traducción
+    const traducciones = {
+        ES: {
+            bienvenida: "Bienvenido",
+            etiquetaTidioma : "Seleccionar Idioma",
+            pregunta: "¿Me podrías decir tu nombre y tu edad?",
+            etiquetaNombre: "Nombre:",
+            etiquetaEdad: "Edad:",
+            boton: "Listo",
+            // Para agregar mas traducciones
+        },
+        EN: {
+            bienvenida: "Welcome",
+            etiquetaTidioma: "Select Language",
+            pregunta: "Could you please tell me your name and age?",
+            etiquetaNombre: "Name:",
+            etiquetaEdad: "Age:",
+            boton: "Submit",
+            // Para Agregar Mas traducciones
+        }
+    };
+    function mostrarMensajeSegunIdioma(nombre, genero, edad, idioma) {
+        const fechaActual = new Date();
+        const horaActual = fechaActual.getHours();
+        const saludoHora = obtenerSaludoHora(horaActual, idioma);
+    
+        mostrarSaludo(nombre, genero, edad, idioma);
+    }
+    function limpiarMensaje() {
+        const mensajeDiv = document.getElementById("mensaje");
+        mensajeDiv.innerHTML = "";
+    }
+    function limpiarPagina() {
+        nombreInput.value = "";
+        edadInput.value = "";
+        limpiarMensaje();
+    }
+
+
+    // Función para cargar el contenido según el idioma seleccionado
+    function cargarContenido(idioma) {
+        limpiarPagina();
+        limpiarMensaje();
+        const textoTraducido = traducciones[idioma];
+        document.title = textoTraducido.bienvenida;
+        document.getElementById("tituloBienvenida").textContent = textoTraducido.bienvenida;
+        document.getElementById("etiquetaTidioma").textContent=textoTraducido.etiquetaTidioma;
+        document.getElementById("pregunta").textContent = textoTraducido.pregunta;
+        document.getElementById("etiquetaNombre").textContent = textoTraducido.etiquetaNombre;
+        document.getElementById("etiquetaEdad").textContent = textoTraducido.etiquetaEdad;
+        document.getElementById("miBoton").textContent = textoTraducido.boton;
+    }
+    
+
+    // Evento para detectar el cambio en el selector de idioma
+    idiomaSelector.addEventListener("change", function () {
+        const idiomaSeleccionado = idiomaSelector.value;
+        cargarContenido(idiomaSeleccionado);
+    });
 
     miBoton.addEventListener("click", async function () {
+        limpiarMensaje();
         const nombre = nombreInput.value;
         const edad = parseInt(edadInput.value); // Convertir la edad a un número
 
         if (nombre.trim() !== "" && !isNaN(edad)) {
             const genero = await determinarGenero(nombre);
-            mostrarSaludo(nombre, genero, edad);
+            const idioma = idiomaSelector.value; // Obtener el idioma seleccionado
+            mostrarSaludo(nombre, genero, edad, idioma);
         }
     });
 
@@ -22,7 +85,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
             if (nombre.trim() !== "" && !isNaN(edad)) {
                 const genero = await determinarGenero(nombre);
-                mostrarSaludo(nombre, genero, edad);
+                const idioma = idiomaSelector.value; // Obtener el idioma seleccionado
+                mostrarSaludo(nombre, genero, edad, idioma);
             }
         }
     });
@@ -46,50 +110,86 @@ document.addEventListener("DOMContentLoaded", function () {
             return "desconocido";
         }
     }
-
-    function obtenerSaludoHora(hora) {
+    function obtenerSaludoHora(hora, idioma) {
         let saludoHora;
-
-        if (hora >= 6 && hora < 12) {
-            saludoHora = "Buenos días";
-        } else if (hora >= 12 && hora < 18) {
-            saludoHora = "Buenas tardes";
+    
+        if (idioma === "ES") {
+            if (hora >= 6 && hora < 12) {
+                saludoHora = "Buenos días";
+            } else if (hora >= 12 && hora < 18) {
+                saludoHora = "Buenas tardes";
+            } else {
+                saludoHora = "Buenas noches";
+            }
+        } else if (idioma === "EN") {
+            if (hora >= 6 && hora < 12) {
+                saludoHora = "Good Morning";
+            } else if (hora >= 12 && hora < 18) {
+                saludoHora = "Good Afternoon";
+            } else {
+                saludoHora = "Good Evening";
+            }
         } else {
-            saludoHora = "Buenas noches";
+            saludoHora = "Hello"; // Saludo predeterminado si no se selecciona ES o EN
         }
-
+    
         return saludoHora;
     }
-
-    function mostrarSaludo(nombre, genero, edad) {
+    
+    function mostrarSaludo(nombre, genero, edad, idioma) {
         const fechaActual = new Date();
         const horaActual = fechaActual.getHours();
-        const saludoHora = obtenerSaludoHora(horaActual);
-
+        const saludoHora = obtenerSaludoHora(horaActual, idioma);
+    
         let saludo;
-
-           if (edad >= 0 && edad <= 10) {
-            if (genero === "hombre") {
-                saludo = "pequeño";
+    
+        if (idioma === "ES") {
+            if (edad >= 0 && edad <= 10) {
+                if (genero === "hombre") {
+                    saludo = "pequeño";
+                } else {
+                    saludo = "pequeña";
+                }
+            } else if (edad >= 11 && edad <= 23) {
+                if (genero === "hombre") {
+                    saludo = "Joven";
+                } else {
+                    saludo = "Señorita";
+                }
+            } else if (genero === "hombre") {
+                saludo = "Señor";
+            } else if (genero === "mujer") {
+                saludo = "Señora";
             } else {
-                saludo = "pequeña";
+                saludo = "persona";
             }
-        } else if (edad >= 11 && edad <= 23) {
-            if (genero === "hombre") {
-                saludo = "Joven";
+            mostrarMensaje(nombre, `${saludoHora}, ${saludo} ${nombre}! ¿En qué puedo ser de ayuda?`);
+        } else if (idioma === "EN") {
+            if (edad >= 0 && edad <= 10) {
+                if (genero === "hombre") {
+                    saludo = "little";
+                } else {
+                    saludo = "little";
+                }
+            } else if (edad >= 11 && edad <= 23) {
+                if (genero === "hombre") {
+                    saludo = "young man";
+                } else {
+                    saludo = "miss";
+                }
+            } else if (genero === "hombre") {
+                saludo = "Mr.";
+            } else if (genero === "mujer") {
+                saludo = "Mrs.";
             } else {
-                saludo = "Señorita";
+                saludo = "person";
             }
-        } else if (genero === "hombre") {
-            saludo = "Señor";
-        } else if (genero === "mujer") {
-            saludo = "Señora";
+            mostrarMensaje(nombre, `${saludoHora}, ${saludo} ${nombre}! How can I help you?`);
         } else {
-            saludo = "persona";
+            saludo = "person"; // Saludo predeterminado si no se selecciona ES o EN
+            mostrarMensaje(nombre, `${saludoHora}, ${saludo} ${nombre}! How can I help you?`);
         }
-
-        mostrarMensaje(nombre, `${saludoHora}, ${saludo} ${nombre}!¿En qué puedo ser de ayuda?`);
     }
+    
 });
-
 
